badfc99e2c58a16aa318d290b0cbbaeae58cab74
unknownaloy <42836070+unknownaloy@users.noreply.github.com>
2022-06-25 16:03:48 +0100

    made the app responsive

--- app.js
+++ app.js
@@ -1,58 +1,55 @@
 let darkMode = localStorage.getItem("darkMode");
-    const darkModeToggle = document.querySelector("#dark-mode-toggle")
-    console.log(darkMode, "DARKMODE");
-    // CHECK IF DARK MODE IS ENABLED
-    //  IF ITS ENABLED TURN IT OFF
-    // IF ITS DISALBED TURN IT ON
-    const enableDarkMode = () => {
-        // 1.ADD THE CLASS DARKMODE TO THE BODY
-        document.body.classList.add("darkmode")
-        // 2.UPDATE DARK MODE IN THE localSTORAGE
-        localStorage.setItem("darkMode", "enabled")
-    }
+const darkModeToggle = document.querySelector("#dark-mode-toggle")
+console.log(darkMode, "DARKMODE");
+// CHECK IF DARK MODE IS ENABLED
+//  IF ITS ENABLED TURN IT OFF
+// IF ITS DISALBED TURN IT ON
+const enableDarkMode = () => {
+    // 1.ADD THE CLASS DARKMODE TO THE BODY
+    document.body.classList.add("darkmode")
+    // 2.UPDATE DARK MODE IN THE localSTORAGE
+    localStorage.setItem("darkMode", "enabled")
+}
 
-    const disableDarkMode = () => {
-        // 1.REMOVE THE CLASS DARKMODE TO THE BODY
-        document.body.classList.remove("darkmode")
-        // 2.
-        localStorage.setItem("darkMode", null)
-    }
-    if (darkMode === "enabled") {
+const disableDarkMode = () => {
+    // 1.REMOVE THE CLASS DARKMODE TO THE BODY
+    document.body.classList.remove("darkmode")
+    // 2.
+    localStorage.setItem("darkMode", null)
+}
+if (darkMode === "enabled") {
+    enableDarkMode()
+}
+
+darkModeToggle.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode")
+
+    if (darkMode !== "enabled") {
         enableDarkMode()
+        console.log(darkMode, "DARK")
+    } else {
+        disableDarkMode()
+        console.log(darkMode, "DARK")
     }
-
-    darkModeToggle.addEventListener("click", () => {
-        darkMode = localStorage.getItem("darkMode")
-
-        if (darkMode !== "enabled") {
-            enableDarkMode()
-            console.log(darkMode, "DARK")
-        } else {
-            disableDarkMode()
-            console.log(darkMode, "DARK")
-        }
-    })
+})
 
 
 window.addEventListener("load", () => {
-    
-    const sunsetValue = document.querySelector(".sun")
+
+    // const sunsetValue = document.querySelector(".sun")
     const humidityDegree = document.querySelector(".humidity")
     const pressureDegree = document.querySelector(".pressure")
     const temperatureDegree = document.getElementById("degree")
     const windDegree = document.querySelector(".wind")
     const area = document.getElementById("city")
-    // const rainDegree = document.querySelector(".rain")
+    const sunriseValue = document.querySelector(".timestamp-sunrise")
+    const sunsetValue =  document.querySelector(".timestamp-sunset")
     const weatherDescription = document.getElementById("weather-description")
 
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=owerri%20&unit=metric&appid=bc2c594876e86589cf23ead605dec442")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=owerri%20&units=metric&appid=bc2c594876e86589cf23ead605dec442")
         .then(response => response.json())
         .then(data => {
             console.log(data, "DATA")
-            const { sunrise } = data.sys
-            sunsetValue.textContent = sunrise.toFixed() + "%"
-
-
             const { humidity, temp, pressure } = data.main
             humidityDegree.textContent = humidity + "%"
             temperatureDegree.textContent = Math.floor(temp) + "⁰C"
@@ -62,14 +59,34 @@ window.addEventListener("load", () => {
             const { description } = data.weather[0]
             weatherDescription.textContent = description
 
+
             const { speed } = data.wind
             windDegree.textContent = speed + "km/h"
 
-            //   const {rain} = data.rain
-            //   rainDegree .textContent = rain
 
             const { name } = data
             area.textContent = name
+
+
+            const { sunrise,sunset } = data.sys
+            const unixTimestamp = sunrise
+            const date = new Date(unixTimestamp * 1000)
+            const hours = date.getHours()
+            const minutes = "0" + date.getMinutes()
+            const seconds = "0" + date.getSeconds()
+            const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2)
+           sunriseValue.textContent = "SUNRISE" + " " + formattedTime
+            console.log(formattedTime)
+
+            const unixSetstamp = sunset
+            const setDate = new Date(unixSetstamp * 1000)
+            const setHours = setDate.getHours()
+            const setMintes = "0" + setDate.getMinutes()
+            const setSeconds = "0" + setDate.getSeconds()
+            const setFormattedTime = setHours + ':' + setMintes.substr(-2) + ':' + setSeconds.substr(-2)
+            sunsetValue.textContent = "SUNSET" + " " + setFormattedTime
+            console.log(setFormattedTime)
+
         })
 
 })
@@ -120,6 +137,7 @@ window.addEventListener("load", () => {
 //     }
 
 // })
+
 
 
 
